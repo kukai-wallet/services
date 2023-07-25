82c94f21f3973f5fcff785764c553c7958ec3ec0
Alex <gounisalex@gmail.com>
2023-07-25 20:00:18 +0200

    fixes categories type, changes content-type header to json

--- src/discover/handle-discover.ts
+++ src/discover/handle-discover.ts
@@ -1,6 +1,11 @@
 import { Env } from '../../worker-configuration'
 import discoverData from './storage/discover_data.json'
 
+const HEADERS = {
+    "content-type": "application/json;charset=UTF-8",
+}
+
 export async function handleDiscover(request: Request, _env: Env, _ctx: ExecutionContext): Promise<Response> {
-    return new Response(JSON.stringify(discoverData, null, 2), { status: 200 })
+    const response = JSON.stringify(discoverData, null, 2)
+    return new Response(response, { headers: HEADERS })
 }
