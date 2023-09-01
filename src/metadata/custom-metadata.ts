f5d9e5e5a88e1f6adc8df47bce45c2e43e367ba5
Alex <gounisalex@gmail.com>
2023-09-01 21:25:11 +0200

    includes dogamí's contract as a contract that needs fallback metadata

--- src/metadata/custom-metadata.ts
+++ src/metadata/custom-metadata.ts
@@ -1,3 +1,4 @@
 export const USE_OBJKT_FOR_CONTRACTS = new Set([
-    'KT1PEGqt5rMmHpyaMXc8RFTFkkAUDrzSFRWk'
+    'KT1PEGqt5rMmHpyaMXc8RFTFkkAUDrzSFRWk',
+    'KT1NVvPsNDChrLRH5K2cy6Sc9r1uuUwdiZQd'
 ])
