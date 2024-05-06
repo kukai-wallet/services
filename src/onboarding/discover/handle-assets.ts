2cd3aedfd2dfa1fd586838e9a99d9a0c711739b6
Alex <gounisalex@gmail.com>
2024-05-06 12:05:41 +0200

    implements onboarding routes, updates content

---
+++ src/onboarding/discover/handle-assets.ts
@@ -0,0 +1,35 @@
+import { ROUTES } from "../../router"
+
+const BASE_PATH = ROUTES.ONBOARDING_DISCOVER_ASSETS
+
+export const REMAP: Record<string, string> = {
+    contract: 'contractAddresses',
+    contractAddress: 'contractAddresses',
+}
+
+const ASSETS = [
+    "https://imagedelivery.net/X6w5bi3ztwg4T4f6LG8s0Q/assets/img/alias/dogami-thumbnail/raw",
+    "https://imagedelivery.net/X6w5bi3ztwg4T4f6LG8s0Q/9dc016fe-e253-4018-f993-b64c04652200/raw",
+    "https://imagedelivery.net/X6w5bi3ztwg4T4f6LG8s0Q/assets/img/alias/mclaren-thumbnail2/raw",
+    "https://imagedelivery.net/X6w5bi3ztwg4T4f6LG8s0Q/assets/img/alias/mufc/raw",
+    "https://imagedelivery.net/X6w5bi3ztwg4T4f6LG8s0Q/assets/img/alias/ziggurats/raw",
+    "https://imagedelivery.net/X6w5bi3ztwg4T4f6LG8s0Q/d59ccc19-894a-4f11-4119-94161e1ed200/raw"
+]
+
+export async function handleAssets(request: Request): Promise<Response> {
+    const { pathname } = new URL(request.url)
+    const assetIndex = pathname.substring(BASE_PATH.length - 1)
+
+    const index = Number(assetIndex)
+
+    if (!Number.isFinite(index)) {
+        return new Response('Malformed URL', { status: 500 })
+    }
+
+    if (index < 0 || index > 5) {
+        return new Response('Index out of bounds', { status: 500 })
+    }
+
+    return Response.redirect(ASSETS[index], 301)
+}
+
--- src/router.ts
+++ src/router.ts
@@ -2,6 +2,7 @@ import { RouteHandler, Router } from 'itty-router';
 import { handleDiscover } from './discover/handle-discover';
 import { handleExpore } from './explore/handle-explore';
 import { handleMetadata } from './metadata/handle-metadata';
+import { handleAssets } from './onboarding/discover/handle-assets';
 import { handleProxy } from './proxy/handle-proxy';
 import { handleVersion } from './version/handle-version';
 
@@ -10,8 +11,12 @@ export enum ROUTES {
 	DISCOVER_V2 = '/v2/discover',
 	DISCOVER_V3 = '/v3/discover',
 	DISCOVER_V4 = '/v4/discover',
+
 	EXPLORE = '/v1/explore',
 	EXPLORE_V2 = '/v2/explore',
+
+	ONBOARDING_DISCOVER_ASSETS = '/v4/onboarding/discover/assets/*',
+
 	METADATA = '/v1/metadata/*',
 	PROXY = '/v1/proxy',
 	VERSION = '/v1/version',
@@ -23,8 +28,12 @@ router.get(ROUTES.DISCOVER, handleDiscover as unknown as RouteHandler)
 router.get(ROUTES.DISCOVER_V2, handleDiscover as unknown as RouteHandler)
 router.get(ROUTES.DISCOVER_V3, handleDiscover as unknown as RouteHandler)
 router.get(ROUTES.DISCOVER_V4, handleDiscover as unknown as RouteHandler)
+
 router.get(ROUTES.EXPLORE, handleExpore as unknown as RouteHandler)
 router.get(ROUTES.EXPLORE_V2, handleExpore as unknown as RouteHandler)
+
+router.get(ROUTES.ONBOARDING_DISCOVER_ASSETS, handleAssets as unknown as RouteHandler)
+
 router.get(ROUTES.METADATA, handleMetadata as unknown as RouteHandler)
 router.get(ROUTES.PROXY, handleProxy as unknown as RouteHandler)
 router.get(ROUTES.VERSION, handleVersion as unknown as RouteHandler)
