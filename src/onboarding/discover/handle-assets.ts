7bfe1081311895af22e09eed04bc21d63dfeed7b
Alex <gounisalex@gmail.com>
2025-07-15 13:05:47 +0300

    replaces onboarding svgs with non-vector assets

--- src/onboarding/discover/handle-assets.ts
+++ src/onboarding/discover/handle-assets.ts
@@ -6,10 +6,10 @@ export const REMAP: Record<string, string> = {
 }
 
 const ASSETS = [
-    "https://imagedelivery.net/X6w5bi3ztwg4T4f6LG8s0Q/fa55796c-cb15-406b-e8d1-77b12bb20200/raw",
+    "https://imagedelivery.net/X6w5bi3ztwg4T4f6LG8s0Q/67773e71-0a53-4fcf-f335-c34aae18b700/raw",
     "https://imagedelivery.net/X6w5bi3ztwg4T4f6LG8s0Q/9dc016fe-e253-4018-f993-b64c04652200/raw",
     "https://imagedelivery.net/X6w5bi3ztwg4T4f6LG8s0Q/16d74d8e-8901-4e16-e9f8-2cf95845b900/raw",
-    "https://imagedelivery.net/X6w5bi3ztwg4T4f6LG8s0Q/28da61cf-cabf-49cb-8a19-3d12ce4c5c00/raw",
+    "https://imagedelivery.net/X6w5bi3ztwg4T4f6LG8s0Q/73dee5f2-2b21-426b-e02c-8e3b92699600/raw",
     "https://imagedelivery.net/X6w5bi3ztwg4T4f6LG8s0Q/assets/img/alias/ziggurats/raw",
     "https://imagedelivery.net/X6w5bi3ztwg4T4f6LG8s0Q/d59ccc19-894a-4f11-4119-94161e1ed200/raw"
 ]
