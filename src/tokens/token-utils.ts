b8e8593ee1ab664ee0010debddb5d7b33e1f97e8
Alex <gounisalex@gmail.com>
2025-12-04 01:17:27 +0100

    switches to getTokensForOwner

--- src/tokens/token-utils.ts
+++ src/tokens/token-utils.ts
@@ -1,4 +1,3 @@
-import { TokenBalanceType } from 'alchemy-sdk';
 import { alchemyEth } from '../constants/alchemy-utils';
 
 export interface Token {
@@ -18,30 +17,24 @@ export async function getEnrichedTokenBalances(address: string): Promise<Token[]
     let pageKey: string | undefined;
 
     do {
-        const response = await alchemyEth.core.getTokenBalances(address, {
-            type: TokenBalanceType.ERC20,
-            pageKey,
-        });
+        const response = await alchemyEth.core.getTokensForOwner(address, { pageKey });
 
-        const enrichedTokens = await Promise.all(
-            response.tokenBalances
-                .filter(token => token.tokenBalance && token.tokenBalance !== '0x' && token.tokenBalance !== '0x0')
-                .map(async (token) => {
-                    const metadata = await alchemyEth.core.getTokenMetadata(token.contractAddress);
-                    const balanceHex = token.tokenBalance!;
+        const enrichedTokens = response.tokens
+            .filter(token => token.rawBalance && token.rawBalance !== '0x' && token.rawBalance !== '0x0')
+            .map((token) => {
+                const balanceHex = token.rawBalance!;
 
-                    return {
-                        contractAddress: token.contractAddress,
-                        balance: BigInt(balanceHex).toString(),
-                        balanceHex,
-                        name: metadata.name,
-                        symbol: metadata.symbol,
-                        decimals: metadata.decimals,
-                        logo: metadata.logo,
-                        type: 'ERC20' as const,
-                    };
-                })
-        );
+                return {
+                    contractAddress: token.contractAddress,
+                    balance: token.balance || BigInt(balanceHex).toString(),
+                    balanceHex,
+                    name: token.name || null,
+                    symbol: token.symbol || null,
+                    decimals: token.decimals ?? null,
+                    logo: token.logo || null,
+                    type: 'ERC20' as const,
+                };
+            });
 
         allAssets.push(...enrichedTokens);
         pageKey = response.pageKey;
